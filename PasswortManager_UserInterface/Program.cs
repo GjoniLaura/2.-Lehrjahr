593575244a269c80b54200d88e56a78c069b4b3c
Laura Gjoni <Laura_Gjoni@sluz.ch>
2023-09-22 13:39:38 +0200

    Versuche

--- PasswortManager_UserInterface/Program.cs
+++ PasswortManager_UserInterface/Program.cs
@@ -8,7 +8,7 @@ namespace PasswortManager_UserInterface
 {
     internal class Program
     {
-        static void Main(string[] args)
+       /* static void Main(string[] args)
         {
             // Masterpasswort Verifizierung
             string Masterpassword = "abc";
@@ -16,27 +16,64 @@ namespace PasswortManager_UserInterface
             int Attemps = 0;
 
             while (Attemps < MaxAttemps)
+            {
 
                 Console.Write("Geben Sie das Masterpasswort ein: ");
-            string Password = Console.ReadLine();
-            if (Password == Masterpassword)
-            {
-                Console.WriteLine("Übersicht Ihrer Passwörter: ");
-                Masterpassword = Convert.ToString(Console.ReadLine());
-                // Passworteinträge anzeigen
-                // URl 
-                // Username und Passwort speichern
-            }
-            else
-            {
-                Attemps = Attemps + 1;
+                string Password = Console.ReadLine();
+                if (Password == Masterpassword)
+                {
+                    Console.WriteLine("Übersicht Ihrer Passwörter: ");
+                    Masterpassword = Convert.ToString(Console.ReadLine());
+                    // Passworteinträge anzeigen
+                    // URl 
+                    // Username und Passwort speichern
+                }
+                else
+                {
+                    Attemps = Attemps + 1;
 
-                Console.WriteLine("Falsche Passworteingabe, versuchen Sie es erneut: ");
+                    Console.WriteLine("Falsche Passworteingabe, versuchen Sie es erneut ({0} von {1}): ", Attemps, MaxAttemps);
+                }
+
             }
-            Console.WriteLine("Falsche Passworteingabe, der Zugriff wird nicht Gewährt");
-            Console.ReadLine();
+
+            if (Attemps == MaxAttemps)
+            {
+                Console.WriteLine("Falsche Passworteingabe, der Zugriff wird nicht Gewährt");
+                Console.ReadLine();
+            }
+        }
+        */
+
+        static void Main()
+        {
+            string MasterPassword = "abc";
+            int MaxAttempts = 2;
+            int Attempts = 0;
+
+            if (Attempts < MaxAttempts)
+            {
+                Console.Write("Bitte geben Sie das Passwort ein: ");
+                string EnteredPassword = Console.ReadLine();
+
+                if (EnteredPassword == MasterPassword)
+                {
+                    Console.WriteLine("Anmeldung erfolgreich!");
+                }
+                else
+                {
+                    Attempts++;
+                    Console.WriteLine("Falsches Passwort. Versuch {0} von {1}", Attempts, MaxAttempts);
+                }
+            }
+
+            if (Attempts == MaxAttempts)
+            {
+                Console.WriteLine("Anmeldung fehlgeschlagen. Sie haben alle Versuche aufgebraucht.");
+            }
         }
     }
 
-    }
+
 }
+
