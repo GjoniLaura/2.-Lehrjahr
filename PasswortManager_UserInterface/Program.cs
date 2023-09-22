d36dce66dcab9ef0e250649bb134b76fa47c0096
Laura Gjoni <Laura_Gjoni@sluz.ch>
2023-09-22 13:26:01 +0200

    Anzahl Versuche

--- PasswortManager_UserInterface/Program.cs
+++ PasswortManager_UserInterface/Program.cs
@@ -10,7 +10,33 @@ namespace PasswortManager_UserInterface
     {
         static void Main(string[] args)
         {
+            // Masterpasswort Verifizierung
+            string Masterpassword = "abc";
+            int MaxAttemps = 2;
+            int Attemps = 0;
+
+            while (Attemps < MaxAttemps)
+
+                Console.Write("Geben Sie das Masterpasswort ein: ");
+            string Password = Console.ReadLine();
+            if (Password == Masterpassword)
+            {
+                Console.WriteLine("Übersicht Ihrer Passwörter: ");
+                Masterpassword = Convert.ToString(Console.ReadLine());
+                // Passworteinträge anzeigen
+                // URl 
+                // Username und Passwort speichern
+            }
+            else
+            {
+                Attemps = Attemps + 1;
+
+                Console.WriteLine("Falsche Passworteingabe, versuchen Sie es erneut: ");
+            }
+            Console.WriteLine("Falsche Passworteingabe, der Zugriff wird nicht Gewährt");
+            Console.ReadLine();
         }
+    }
 
     }
 }
