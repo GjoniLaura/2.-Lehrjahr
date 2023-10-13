ce02b1c85ca0aed6aed86d18a2122ccab82e88fa
aurel.bieri <bieri.aurel@gmail.com>
2023-10-13 20:23:45 +0200

    added the basic main-menu

---
+++ PasswortManager_UserInterface/Menu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswortManager_UserInterface
+{
+    internal class Menu
+    {
+        public static string titel = "Passwortmanager\n" +
+                           "-----------------------------------------------";
+        public static void mainmenu()
+        {
+            Console.Clear();
+            int dec;
+
+            do
+            {
+                Console.WriteLine(titel);
+                Console.WriteLine("Was möchtest du tun?");
+                Console.WriteLine("1.Alle meine Passwörter anzeigen lassen");
+                Console.WriteLine("2.Ein neues Passwort hinzufügen");
+                Console.WriteLine("3.Ein Passwort Löschen");
+                Console.WriteLine("4.Passwort generieren lassen");
+                Console.WriteLine("5.Passwort mit url/webseitename suchen");
+                Console.WriteLine("6.Benutzer Account Löschen");
+                Console.WriteLine("7.Exit");
+                Console.Write("\nEingabe:");
+
+                if (int.TryParse(Console.ReadLine(), out int input) && input >= 1 && input <= 7)
+                {
+                    dec = input;
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl zwischen 1 und 7 ein.\n");
+                }
+            } while (true);
+
+            switch (dec) 
+            {
+               case 1:
+                    showpasswords();
+                    break;
+               case 2:
+                    addpassword();
+                    break;
+
+            }
+        }
+
+        static void showpasswords()
+        {
+
+        }
+
+        static void addpassword()
+        {
+            string password_titel = "";
+            string password = "";
+            string place = "";
+            string username = "";
+
+            Console.Clear();
+            Console.WriteLine(titel);
+            Console.WriteLine("Neues Passwort hinzufügen\n");
+
+            Console.Write("Titel des Passworteintrags: ");
+            password_titel = Console.ReadLine();
+
+            Console.WriteLine("");
+
+
+        }
+    }
+}
--- PasswortManager_UserInterface/PasswordManager.cs
+++ PasswortManager_UserInterface/PasswordManager.cs
@@ -99,6 +99,7 @@ namespace PasswortManager_UserInterface
             string titel = "Login\n----------------------------------------------------------";
             int counter = 0;
             int anztry = 3;
+
             Console.WriteLine(titel);
             do
             {
@@ -119,7 +120,7 @@ namespace PasswortManager_UserInterface
                 masterpassword = Console.ReadLine();
                 if (ValidatePassword(masterpassword, founduser.masterpassword))
                 {
-
+                    Menu.mainmenu();
                 }
                 else
                 {
@@ -140,44 +141,6 @@ namespace PasswortManager_UserInterface
         {
             return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
         }
-
-
-        public string Passwordgenerator(int len)
-        {
-            int lenthOfPassword = len;
-            string password = "";
-            int count = 0;
-            int currand = 0;
-
-            string letters_Lowercase = "abcdefghijklmnopqrstuvwxyz";
-            string letters_Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string symbols = @"!#$?*@\";
-
-            Random ran = new Random();
-
-            while (count < lenthOfPassword)
-            {
-                currand = ran.Next(0, 4);
-                switch (currand)
-                {
-                    case 0:
-                        password += letters_Lowercase[ran.Next(letters_Lowercase.Length)];
-                        break;
-                    case 1:
-                        password += letters_Uppercase[ran.Next(letters_Uppercase.Length)];
-                        break;
-                    case 2:
-                        password += symbols[ran.Next(symbols.Length)];
-                        break;
-                    case 3:
-                        password += Convert.ToString(ran.Next(0, 9));
-                        break;
-                }
-                count++;
-            }
-            return password;
-        }
-
 
 
 
