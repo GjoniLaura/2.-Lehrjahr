f8e8eeff214c10cd545e88b466c7413cd20eb3b6
Aurel Bieri <aurel.bieri@gmail.com>
2023-11-30 20:50:34 +0100

    Added the ClockTime Class

---
+++ TimeTable/ClockTimes.cs
@@ -0,0 +1,37 @@
+﻿namespace TimeTable
+{
+    public class ClockTimes
+    {
+        //Diese Klasse muss nochmals überarbeitet werden sobalt wir 
+        //mehr gedankten über den Algorythmus und der eingabe der Daten 
+        //gemacht haben.
+
+        private string Bezeichnung { get; set; }
+        private int Hour;
+        private int Minutes;
+
+        public ClockTimes(int Hour, int Minutes, string bezeichnung)
+        {
+            SetHour(Hour);
+            SetMinutes(Minutes);
+            Bezeichnung = bezeichnung;
+        }
+        public void SetHour(int Hour)
+        {
+            this.Hour = Hour;
+        }
+        public int GetHour()
+        {
+            return Hour;
+        }
+        public void SetMinutes(int Minutes)
+        {
+            this.Minutes = Minutes;
+        }
+        public int GetMinutes()
+        {
+            return Minutes;
+        }
+
+    }
+}
