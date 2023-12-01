f4bc2b3e21efff0e4bb8263131a482aef5018b1c
Laura Gjoni <Laura_Gjoni@sluz.ch>
2023-12-01 13:52:34 +0100

    Subject Klasse

---
+++ TimeTable/Education.cs
@@ -0,0 +1,17 @@
+﻿namespace TimeTable
+{
+    public class Education
+    {
+        public string _participant { get; set; }
+        public string _subject { get; set; }
+        public int _lessons { get; set; }
+
+
+        public Education(string participant, string subject, int lessons)
+        {
+            _participant = participant;
+            _subject = subject;
+            _lessons = lessons;
+        }
+    }
+}
--- TimeTable/Subject.cs
+++ TimeTable/Subject.cs
@@ -2,5 +2,18 @@
 {
     public class Subject
     {
+        public string _designation { get; set; }
+        public string _description { get; set; }
+        public int _Id { get; set; }
+        public string _premises { get; set; }
+
+
+        public Subject(string designation, string description, int Id, string premises)
+        {
+            _designation = designation;
+            _description = description;
+            _Id = Id;
+            _premises = premises;
+        }
     }
 }
