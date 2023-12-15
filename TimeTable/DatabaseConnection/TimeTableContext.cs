082b2dd2dda4c96a65edde746d2dfd07cdf7c818
Aurel Bieri <aurel.bieri@gmail.com>
2023-12-15 21:29:11 +0100

    an connection zu db weitergearbeitet, funktioniert nocht nicht

--- TimeTable/DatabaseConnection/TimeTableContext.cs
+++ TimeTable/DatabaseConnection/TimeTableContext.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql;
 
 namespace TimeTable.DatabaseConnection
 {
     public class TimeTableContext : DbContext
     {
-        public DbSet<Subject> Subjects { get; set; }
+        string connectionstring = "server = localhost;  database = timetable; uid = root; pwd = Luna07wenn!";
+
+		public DbSet<Subject> Subjects { get; set; }
         public DbSet<Student> Addresses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("server = localhost; database = timetable; uid = root; pwd = Luna07wenn!", ServerVersion.AutoDetect("server = localhost; database = timetable; uid = root; pwd = Luna07wenn!"));
+            optionsBuilder.UseMySql(connectionstring, ServerVersion.AutoDetect(connectionstring));
         }
 
         public void InitializeDatabase()
