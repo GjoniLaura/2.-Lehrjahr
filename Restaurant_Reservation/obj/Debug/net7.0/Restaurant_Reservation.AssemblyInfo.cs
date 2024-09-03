7f98f3793cad0ad1ac6afbcf20d665b7170b8013
Laura Gjoni <Laura_Gjoni@sluz.ch>
2024-09-03 08:46:58 +0200

    Initial commit

--- Restaurant_Reservation/obj/Debug/net7.0/Restaurant_Reservation.AssemblyInfo.cs
+++ Restaurant_Reservation/obj/Debug/net7.0/Restaurant_Reservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant_Reservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd96bfe349249ad60d48bae8626a97e9d11ea73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant_Reservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant_Reservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
